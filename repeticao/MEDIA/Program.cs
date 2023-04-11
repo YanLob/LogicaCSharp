a89f73e1dbb668db15ef64919f0b56c76c6c5d9b
2022103030015 <yanlobate@gmail.com>
2023-04-11 11:43:46 -0400

    aaa

---
+++ repeticao/MEDIA/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEDIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int cont = 1;
+            int bimestre = 1;
+            int mediaAluno = 0;
+            int mediaTurma = 0;
+            int nota = 0;
+            Console.Write("Digite o total de alunos na turma: ");
+            int alunos = Convert.ToInt32(Console.ReadLine());
+
+            while (cont <= alunos)
+            {
+                while(bimestre <= 4)
+                {
+                    Console.Write($"Digite a nota {cont}° do aluno no {bimestre }°: ");
+                    nota += Convert.ToInt32(Console.ReadLine());
+                    bimestre++;
+
+                }
+                if (bimestre > 4)
+                {   
+                    mediaAluno += nota / 4;
+                    Console.WriteLine($"A média do {cont}° é {mediaAluno}");
+
+                    mediaTurma += mediaAluno;
+                    Console.WriteLine("Agr outro aluno");
+                    bimestre = 1;
+                    nota = 0;
+                    
+                }
+                cont++;
+            }
+            
+        }
+    }
+}
---
+++ repeticao/repeticao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 1;
+            while(op == 1)
+            {
+                Console.WriteLine("---Calculadora com While---");
+
+                Console.Write("Digite um valor para sua tabuada: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                int cont = 0;
+
+                while (cont <= 10)
+                {
+                    Console.WriteLine($"{num} x {cont} = {num * cont}");
+                    cont++;
+                }
+                Console.WriteLine("Deseja continuar?[1 para SIM | 2 para NÃO]");
+                op = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.WriteLine("Programa finalizado");
+        }
+    }
+}

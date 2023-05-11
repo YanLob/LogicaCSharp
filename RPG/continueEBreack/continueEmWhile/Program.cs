5f68b4e01de636dba9c2d2f1596935b71fedc04e
YanLob <yanlobate@gmail.com>
2023-05-10 23:45:52 -0300

    .

---
+++ RPG/continueEBreack/continueEmWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace continueEmWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite um numero: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if(num<=10 || num >= 20)
+                {
+                    continue;
+                }
+                else
+                {
+                    if(num==15){
+                        break;
+                    }
+                    Console.WriteLine("Numero: "+num);
+                }
+            }
+        }
+    }
+}

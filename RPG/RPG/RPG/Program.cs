1cae152f0e19637e88650d85563768df9c1859dc
YanLob <yanlobate@gmail.com>
2023-05-10 22:41:41 -0300

    .

--- RPG/RPG/RPG/Program.cs
+++ RPG/RPG/RPG/Program.cs
@@ -1,54 +1,107 @@
 ﻿using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
 using System.Text;
+
 using System.Threading.Tasks;
 
+
+
 namespace RPG
+
 {
+
     internal class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
             int dano = 0, vMonstro = 10, vHerio = 10;
 
+
+
             Random random = new Random();
+
             do
+
             {
+
                 dano = random.Next(1, 10);
+
                 vMonstro -= dano;
-                 
+
+
+
                 if (vMonstro < 0)
+
                 {
+
                     vMonstro = 0;
+
                 }
+
                 Console.WriteLine($"O MONSTRO foi danificado e perdeu {dano} de saúde e agora tem {vMonstro} HP");
 
+
+
                 if (vMonstro > 0)
+
                 {
+
                     dano = random.Next(1, 10);
+
                     vHerio -= dano;
 
+
+
                     if (vHerio < 0)
+
                     {
+
                         vHerio = 0;
+
                     }
+
                     Console.WriteLine($"O HEROI foi danificado e perdeu {dano} de saúde e agora tem {vHerio} HP");
+
                 }
+
+
+
 
 
             } while (vHerio > 0 && vMonstro > 0);
 
+
+
             if (vHerio > 0)
+
             {
+
                 Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
                 Console.WriteLine($"O HEROI venceu com {vHerio} de HP!");
+
             }
+
             else
+
             {
+
                 Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
                 Console.WriteLine($"O MONSTRO venceu com {vMonstro} de HP!");
+
             }
+
         }
+
     }
+
 }
---
+++ RPG/continueEBreack/continueEBreack/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace continueEBreack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma=0;
+            Random gerador = new Random();
+
+            Console.WriteLine("---Continues---");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Digite o {i}° numero: ");
+                int num = (int)(gerador.NextDouble()*6+1);
+                Console.WriteLine("numero: "+num);
+                if (num % 2 != 0)
+                {
+                    continue;
+                }
+                soma += num;
+                Console.WriteLine("soma: " + soma);
+                Console.WriteLine("ABBA");
+
+                if(soma>10) {
+                    Console.WriteLine("contunue deu certo");
+                    break;
+                }
+            }
+            Console.WriteLine("Encerrado.");
+        }
+    }
+}

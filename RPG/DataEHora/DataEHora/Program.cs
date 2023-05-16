67e8366fe50742b6352cea6d1da3c5650901e16f
YanLob <yanlobate@gmail.com>
2023-05-16 18:05:46 -0300

    Usando DateTime

---
+++ RPG/DataEHora/DataEHora/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEHora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Cálculo de idade---");
+            Console.Write("Digite seu ano de nascimento");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            int dataNasc = DateTime.Now.Year - ano;
+
+            Console.WriteLine($"Você tem ou vai fazer {dataNasc} anos.");
+        }
+    }
+}

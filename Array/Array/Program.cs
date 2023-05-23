1da37a87677564d71eca870e6774917f2e6d2675
2022103030015 <yanlobate@gmail.com>
2023-05-23 10:49:02 -0400

    ababa

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -10,6 +10,14 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            int mult = 0;
+
+            int[] nuns = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                nuns[i] = i * 2;
+                Console.WriteLine(nuns[i]);
+            }
         }
     }
 }

8d64f1b9b11e007b547187c0b30be928b02203fd
2022103030015 <yanlobate@gmail.com>
2023-05-25 15:04:34 -0400

    teste

---
+++ Array/teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[] {1,2,22,4,20,6,7,99,92,100};
+
+            Console.WriteLine(numeros.Contains(100));
+        }
+    }
+}

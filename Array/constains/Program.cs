989f690282122556b24f478798dfd9855f234474
YanLob <yanlobate@gmail.com>
2023-06-01 10:54:41 -0300

    exercicio 2

---
+++ Array/constains/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[10];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write($"Digite o {i+1}° valor: ");
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Digite um valor para ver se ele existe no array: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+            if (A.Contains(valor))
+            {
+                Console.WriteLine("ACHEI");
+            }
+            else {Console.WriteLine("NÃO ACHEI");}
+        }
+    }
+}

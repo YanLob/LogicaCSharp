832fca3179370e4beeed5b70463f8d51c535d34c
2022103030015 <yanlobate@gmail.com>
2023-06-06 11:33:08 -0400

    Matriz

--- LogicaCSharp/Matriz/Matriz/Program.cs
+++ LogicaCSharp/Matriz/Matriz/Program.cs
@@ -10,6 +10,24 @@ namespace Matriz
     {
         static void Main(string[] args)
         {
+            int[,] notas = new int[3, 4];
+            int aluno = 1, total = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write($"Digite a nota do {j+1}° bimestre do {aluno}° aluno: ");
+                    notas[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                    total += notas[i, j];
+                }
+                aluno++;
+                Console.Clear();
+            }
+
+            int media = total / 12;
+            Console.WriteLine("A média da turma é -> " + media);
+            Console.ReadKey();
         }
     }
 }

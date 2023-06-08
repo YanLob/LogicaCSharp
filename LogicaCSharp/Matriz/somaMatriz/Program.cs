be2a13e69da0b99fd109b5b5399bf3f421cf163e
YanLob <yanlobate@gmail.com>
2023-06-08 18:26:22 -0300

    começando exercicio em matriz

---
+++ LogicaCSharp/Matriz/somaMatriz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somaMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4, 5];
+            int soma = 0, num;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("--------------------");
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("digite um valor: ");
+                    num = Convert.ToInt32(Console.ReadLine());
+
+                    soma = soma + num;
+                }
+                
+            }
+
+            Console.WriteLine("A soma das colunas sao" + soma);
+        }
+    }
+}

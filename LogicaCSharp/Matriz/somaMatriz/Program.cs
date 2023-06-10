ff001c96d48efce19ee3c83b6775106efc43f7fb
YanLob <yanlobate@gmail.com>
2023-06-09 23:52:01 -0300

    somaMatriz

--- LogicaCSharp/Matriz/somaMatriz/Program.cs
+++ LogicaCSharp/Matriz/somaMatriz/Program.cs
@@ -10,23 +10,39 @@ namespace somaMatriz
     {
         static void Main(string[] args)
         {
-            int[,] array = new int[4, 5];
-            int soma = 0, num;
+            // Definindo a matriz 4x5
+            int[,] matriz = new int[4, 5];
 
+            // Lendo os valores da matriz
             for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("--------------------");
                 for (int j = 0; j < 5; j++)
                 {
-                    Console.Write("digite um valor: ");
-                    num = Convert.ToInt32(Console.ReadLine());
-
-                    soma = soma + num;
+                    Console.Write("Digite o valor para a posição [{0},{1}]: ", i, j);
+                    matriz[i, j] = int.Parse(Console.ReadLine());
                 }
-                
             }
 
-            Console.WriteLine("A soma das colunas sao" + soma);
+            // Calculando a soma dos elementos de cada coluna
+            int[] somaColunas = new int[5];
+
+            for (int j = 0; j < 5; j++)
+            {
+                int soma = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    soma += matriz[i, j];
+                }
+                somaColunas[j] = soma;
+            }
+
+            // Exibindo o resultado
+            Console.WriteLine("\nSoma dos elementos de cada coluna:");
+
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine("Coluna {0}: {1}", j + 1, somaColunas[j]);
+            }
         }
     }
 }

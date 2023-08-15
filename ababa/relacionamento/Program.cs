498b3fd3316c5f0fe82ff1463a29b5f0fe51e0ff
Yan Fernando <yanlobate@gmail.com>
2023-08-15 08:32:26 -0400

    a

---
+++ ababa/relacionamento/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace relacionamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Agencia agencia = new Agencia();
+            agencia.numero = 1825;
+            agencia.nome = "Abababab";
+
+            Conta c = new Conta();
+            c.numeroDaConta = 1239175123;
+            c.agencia = agencia;
+            c.agencia = agencia;
+
+            Console.WriteLine(c.agencia.numero);
+            Console.WriteLine(c.agencia.nome);
+        }
+    }
+}
